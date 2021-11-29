8f83e004a4d56d3559c78f545eb8f6d2230bc191
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-11-29 10:00:01 +0000

    Fetch data

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [text, setText] = useState("");
+
+	useEffect(() => {
+		const url =
+			"https://assets.breatheco.de/apis/fake/todos/user/danieloos";
+		const parameters = {
+			method: "GET",
+			contentType: "application/json"
+		};
+
+		fetch(url, parameters)
+			.then(res => res.json())
+			.then(setText)
+			.catch(err => setText(err.toString()));
+	}, []);
+
+	return <div className="text-center mt-5">{text}</div>;
 };
 
 export default Home;
